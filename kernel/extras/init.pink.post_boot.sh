80b3c510bed3a1a753b3421ea9e2d6fd10d3dbd5
Luka Karinja <lk@varjanta.com>
2014-04-15 10:18:36 +0200

    20140415

--- kernel/extras/init.pink.post_boot.sh
+++ kernel/extras/init.pink.post_boot.sh
@@ -1,7 +1,11 @@
 #!/system/bin/sh
 
+############################
+# various tweaks from CM
+#
 target=`getprop ro.board.platform`
 debuggable=`getprop ro.debuggable`
+emmc_boot=`getprop ro.boot.emmc`
 
 echo 2 > /sys/module/lpm_resources/enable_low_power/l2
 soc_revision=`cat /sys/devices/soc0/revision`
@@ -14,6 +18,16 @@ if [ "$product" == "falcon" ]; then
 		echo 1 > /sys/kernel/debug/clk/cxo_lpm_clk/enable
 	fi
 fi
+
+case "$emmc_boot"
+    in "true")
+        chown -h system /sys/devices/platform/rs300000a7.65536/force_sync
+        chown -h system /sys/devices/platform/rs300000a7.65536/sync_sts
+        chown -h system /sys/devices/platform/rs300100a7.65536/force_sync
+        chown -h system /sys/devices/platform/rs300100a7.65536/sync_sts
+    ;;
+esac
+
 echo 1 > /sys/module/msm_pm/modes/cpu0/power_collapse/suspend_enabled
 echo 1 > /sys/module/msm_pm/modes/cpu1/power_collapse/suspend_enabled
 echo 1 > /sys/module/msm_pm/modes/cpu2/power_collapse/suspend_enabled
@@ -31,8 +45,8 @@ echo 1 > /sys/devices/system/cpu/cpu1/online
 echo 1 > /sys/devices/system/cpu/cpu2/online
 echo 1 > /sys/devices/system/cpu/cpu3/online
 chmod 664 /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-chown -h system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-chown -h system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+#chown -h system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+#chown -h system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 chown -h root.system /sys/devices/system/cpu/cpu1/online
 chown -h root.system /sys/devices/system/cpu/cpu2/online
 chown -h root.system /sys/devices/system/cpu/cpu3/online
@@ -40,14 +54,19 @@ chmod -h 664 /sys/devices/system/cpu/cpu1/online
 chmod -h 664 /sys/devices/system/cpu/cpu2/online
 chmod -h 664 /sys/devices/system/cpu/cpu3/online
 
-echo "intelliactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+echo 100000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
 
+############################
+# Intelliplug activate
+#
 echo 1 > /sys/module/intelli_plug/parameters/intelli_plug_active
 
 ############################
-# mount debugfs
+# dynamic timer slack
 #
-mount -t debugfs nodev /sys/kernel/debug
+echo 100000 > /sys/fs/cgroup/timer_slack.min_slack_suspend_ns
 
 ############################
 # Tweak UKSM
@@ -68,7 +87,7 @@ echo 25 > /sys/kernel/mm/uksm/max_cpu_percentage
 ############################
 # Tweak ZSWAP
 #
-echo 70 > /proc/sys/vm/swappiness
+echo 60 > /proc/sys/vm/swappiness
 
 ############################
 # Init VNSWAP
@@ -80,8 +99,6 @@ swapon /dev/block/vnswap0
 ############################
 # pink tweaks
 #
-echo 8 > /proc/sys/vm/page-cluster
-echo 1 > /proc/sys/kernel/multi_threading
 echo 2 > /sys/devices/system/cpu/sched_mc_power_savings
 
 ############################
@@ -126,15 +143,4 @@ echo "4096 39000 187000" > /proc/sys/net/ipv4/tcp_wmem
 echo "187000 187000 187000" > /proc/sys/net/ipv4/tcp_mem
 echo 1 > /proc/sys/net/ipv4/tcp_no_metrics_save
 echo 1 > /proc/sys/net/ipv4/tcp_moderate_rcvbuf
-
-emmc_boot=`getprop ro.boot.emmc`
-case "$emmc_boot"
-    in "true")
-        chown -h system /sys/devices/platform/rs300000a7.65536/force_sync
-        chown -h system /sys/devices/platform/rs300000a7.65536/sync_sts
-        chown -h system /sys/devices/platform/rs300100a7.65536/force_sync
-        chown -h system /sys/devices/platform/rs300100a7.65536/sync_sts
-    ;;
-esac
-
 
